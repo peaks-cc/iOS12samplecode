82850a062e83edbf9e264b4bd9f5219f4fb8f24f
sonson <yoshida.yuichi@gmail.com>
2018-07-24 01:36:10 +0900

    Pythonのコードを修正．結果の出力ファイルも一緒に修正． MLRegressorのサンプルPlaygroundsをちょっと修正．

--- 06/Create ML/regression.playground/Contents.swift
+++ 06/Create ML/regression.playground/Contents.swift
@@ -3,16 +3,14 @@ import Cocoa
 
 func classify(path: String) {
     do {
-        let url = URL(fileURLWithPath: "/Users/sonson/Downloads/winequality-white.csv")
+        let url = URL(fileURLWithPath: path)
         let data = try MLDataTable(contentsOf: url)
         let (trainingData, testData) = data.randomSplit(by: 0.8, seed: 0)
-        
-        do {
-            let regressor = try MLClassifier(trainingData: trainingData, targetColumn: "quality")
-            let metrics = regressor.evaluation(on: testData)
-            print(metrics)
-        }
-        
+
+        let regressor = try MLClassifier(trainingData: trainingData, targetColumn: "quality")
+        let metrics = regressor.evaluation(on: testData)
+        print(metrics)
+
     } catch {
         print(error)
     }
@@ -20,22 +18,18 @@ func classify(path: String) {
 
 func regress(path: String) {
     do {
-        let url = URL(fileURLWithPath: "/Users/sonson/Downloads/winequality-white.csv")
+        let url = URL(fileURLWithPath: path)
         let data = try MLDataTable(contentsOf: url)
         let (trainingData, testData) = data.randomSplit(by: 0.8, seed: 0)
-        
-        do {
-            let regressor = try MLRegressor(trainingData: trainingData, targetColumn: "quality")
-            let metrics = regressor.evaluation(on: testData)
-            print(metrics)
-        }
-        
+        let regressor = try MLRegressor(trainingData: trainingData, targetColumn: "quality")
+        let metrics = regressor.evaluation(on: testData)
+        print(metrics)
     } catch {
         print(error)
     }
 }
 
-regress(path: "/Users/sonson/Library/Mobile Documents/com~apple~CloudDocs/code/coremlSample/boston.csv")
-classify(path: "/Users/sonson/Library/Mobile Documents/com~apple~CloudDocs/code/coremlSample/winequality-white.csv")
-classify(path: "/Users/sonson/Library/Mobile Documents/com~apple~CloudDocs/code/coremlSample/winequality-red.csv")
+regress(path: "/Users/sonson/Documents/code/iOS12samplecode/06/dataset/boston.csv")
+classify(path: "/Users/sonson/Documents/code/iOS12samplecode/06/dataset/winequality-white.csv")
+classify(path: "/Users/sonson/Documents/code/iOS12samplecode/06/dataset/winequality-red.csv")
 
